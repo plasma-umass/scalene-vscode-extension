0cbe659eefc03c0d37e71f84633b7299cc7919cd
Emery Berger <emery.berger@gmail.com>
2023-09-12 17:16:51 -0400

    Added support for navigation from Scalene's web view to the source code.

--- extension.js
+++ extension.js
@@ -1,151 +1,198 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
-const child_process = require('child_process');
-const path = require('path');
-const os = require('os');
-const fs = require('fs');
+const vscode = require("vscode");
+const child_process = require("child_process");
+const path = require("path");
+const os = require("os");
+const fs = require("fs");
 
 async function getBestPythonPath() {
-    // Check if Python extension is installed and active
-    const pythonExtension = vscode.extensions.getExtension('ms-python.python');
-    
-    if (pythonExtension && pythonExtension.isActive) {
-        const pythonConfig = vscode.workspace.getConfiguration('python');
-        const pythonPathFromExtension = pythonConfig.get('pythonPath');
+  // Check if Python extension is installed and active
+  const pythonExtension = vscode.extensions.getExtension("ms-python.python");
 
-        if (pythonPathFromExtension) {
-            return pythonPathFromExtension;
-        }
+  if (pythonExtension && pythonExtension.isActive) {
+    const pythonConfig = vscode.workspace.getConfiguration("python");
+    const pythonPathFromExtension = pythonConfig.get("pythonPath");
+
+    if (pythonPathFromExtension) {
+      return pythonPathFromExtension;
     }
+  }
 
-    // Fallback to using the environment
-    try {
-        const platform = process.platform;
+  // Fallback to using the environment
+  try {
+    const platform = process.platform;
 
-        if (platform === 'win32') {
-            // On Windows, use 'where'
-            const pythonPath = child_process.execSync('where python').toString().split('\r\n')[0];
-            if (pythonPath.includes('Python3') || pythonPath.includes('python3')) {
-                return pythonPath;
-            }
-        } else {
-            // On Linux/Mac, use 'which'
-            const pythonPath = child_process.execSync('which python3').toString().trim();
-            return pythonPath;
-        }
-    } catch (error) {
-        console.error('Error finding Python path:', error);
+    if (platform === "win32") {
+      // On Windows, use 'where'
+      const pythonPath = child_process
+        .execSync("where python")
+        .toString()
+        .split("\r\n")[0];
+      if (pythonPath.includes("Python3") || pythonPath.includes("python3")) {
+        return pythonPath;
+      }
+    } else {
+      // On Linux/Mac, use 'which'
+      const pythonPath = child_process
+        .execSync("which python3")
+        .toString()
+        .trim();
+      return pythonPath;
     }
+  } catch (error) {
+    console.error("Error finding Python path:", error);
+  }
 
-    // If everything fails, return a default (this might not always be accurate)
-    return 'python3';
+  // If everything fails, return a default (this might not always be accurate)
+  return "python3";
 }
 
 /**
- * 
- * @param {string} currentFilePath 
+ *
+ * @param {string} currentFilePath
  */
 
-function runScalene(currentFilePath) {
-	const dismissButton = { title: "Dismiss" };
-	vscode.window.showInformationMessage('Scalene: now profiling ' + currentFilePath, dismissButton);
+function runScalene(currentFilePath, context) {
+  const dismissButton = { title: "Dismiss" };
+  vscode.window.showInformationMessage(
+    "Scalene: now profiling " + currentFilePath,
+    dismissButton,
+  );
 
-	// Create a unique temp directory for our extension
-	const tempDir = path.join(os.tmpdir(), 'scalene_' + Date.now());
-	fs.mkdirSync(tempDir);
+  // Create a unique temp directory for our extension
+  const tempDir = path.join(os.tmpdir(), "scalene_" + Date.now());
+  fs.mkdirSync(tempDir);
 
-	const outputFilename = `${tempDir}/profile-${process.pid}.html`;
+  const outputFilename = `${tempDir}/profile-${process.pid}.html`;
 
-    let executablePath = 'python3';
-    
-    (async () => {
-	executablePath = await getBestPythonPath();
-    })();
-    
-	const args = ['-m', 'scalene', '--no-browser', '--outfile', outputFilename, '---', currentFilePath];  // replace with your arguments
-	const proc = child_process.spawn(executablePath, args);
+  let executablePath = "python3";
 
-	// Redirect stdout to the Output pane ("channel")
-	const outputChannel = vscode.window.createOutputChannel("Output");
-	// Show the output pane to the user
-	outputChannel.show();
+  (async () => {
+    executablePath = await getBestPythonPath();
+  })();
 
-	proc.stdout.on('data', (data) => {
-		outputChannel.appendLine(`${data}`);
-	});
+  const args = [
+    "-m",
+    "scalene",
+    "--no-browser",
+    "--outfile",
+    outputFilename,
+    "---",
+    currentFilePath,
+  ]; // replace with your arguments
+  const proc = child_process.spawn(executablePath, args);
 
-	if (false) { // disabled for now
-		proc.stdout.on('data', (data) => {
-			vscode.window.showInformationMessage(`STDOUT: ${data}`);
-		});
-		
-		proc.stderr.on('data', (data) => {
-			vscode.window.showWarningMessage(`STDERR: ${data}`);
-		});
-	}
+  // Redirect stdout to the Output pane ("channel")
+  const outputChannel = vscode.window.createOutputChannel("Output");
+  // Show the output pane to the user
+  outputChannel.show();
 
-	proc.on('close', (code) => {
-		const dismissButton = { title: "Dismiss" };
-		if (code !== 0) {
-			vscode.window.showErrorMessage(`Scalene: process exited with code: ${code}`);
-		} else {
-			// vscode.window.showInformationMessage(`Scalene: done profiling ${currentFilePath}`, dismissButton);
-			const panel = vscode.window.createWebviewPanel(
-				'scaleneView', 
-				// outputFilename,
-				`Scalene: ${currentFilePath}`,
-				vscode.ViewColumn.One,
-				{ 	enableScripts: true,
-					enableCommandUris: true,
-					allowSameOriginForContent: true,
-					//localResourceRoots: [vscode.Uri.file(path.join(__dirname, 'media'))]
-					//retainContextWhenHidden: true, // This will ensure WebView is not reset when hidden.
-				 }
-			);
-		    // outputChannel.appendLine(outputFilename);
-		    let content = fs.readFileSync(outputFilename, 'utf-8');
-		    panel.webview.html = content;
-		}
-	});
-	
+  proc.stdout.on("data", (data) => {
+    outputChannel.appendLine(`${data}`);
+  });
+
+  if (false) {
+    // disabled for now
+    proc.stdout.on("data", (data) => {
+      vscode.window.showInformationMessage(`STDOUT: ${data}`);
+    });
+
+    proc.stderr.on("data", (data) => {
+      vscode.window.showWarningMessage(`STDERR: ${data}`);
+    });
+  }
+
+  let panel;
+
+  proc.on("close", (code) => {
+    if (code !== 0) {
+      vscode.window.showErrorMessage(
+        `Scalene: process exited with code: ${code}`,
+      );
+      return;
+    }
+    panel = vscode.window.createWebviewPanel(
+      "scaleneView",
+      // outputFilename,
+      `Scalene: ${currentFilePath}`,
+      vscode.ViewColumn.One,
+      {
+        enableScripts: true,
+        enableCommandUris: true,
+        allowSameOriginForContent: true,
+        //localResourceRoots: [vscode.Uri.file(path.join(__dirname, 'media'))]
+        //retainContextWhenHidden: true, // This will ensure WebView is not reset when hidden.
+      },
+    );
+    context.subscriptions.push(
+      // Handle messages from the webview
+      panel.webview.onDidReceiveMessage(
+        async (message) => {
+          switch (message.command) {
+            case "jumpToLine":
+              const filePath = message.filePath;
+              const lineNumber = message.lineNumber - 1;
+
+              const documentUri = vscode.Uri.file(filePath);
+              const document =
+                await vscode.workspace.openTextDocument(documentUri);
+              const editor = await vscode.window.showTextDocument(document);
+
+              const line = Math.min(lineNumber, document.lineCount - 1);
+              const position = new vscode.Position(line, 0);
+              editor.selection = new vscode.Selection(position, position);
+              editor.revealRange(new vscode.Range(position, position));
+
+              break;
+          }
+        },
+        undefined,
+        context.subscriptions,
+      ),
+    );
+    // outputChannel.appendLine(outputFilename);
+    let content = fs.readFileSync(outputFilename, "utf-8");
+    panel.webview.html = content;
+  });
 }
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
+  let disposable = vscode.commands.registerCommand(
+    "scalene.profile",
+    function () {
+      const currentEditor = vscode.window.activeTextEditor;
+      let currentFilePath = "";
 
-	let disposable = vscode.commands.registerCommand('scalene.profile', function () {
-		const currentEditor = vscode.window.activeTextEditor;
-		let currentFilePath = "";
-	
-		if (currentEditor) {
-			currentFilePath = currentEditor.document.uri.fsPath;
-		} else {
-			return;
-		}
-	
-		// Check whether the file ends in ".py"
-		let isPython = currentFilePath.endsWith(".py");
-	
-		// Display a message box to the user
-		if (isPython) {
-//			vscode.window.showInformationMessage('Scalene: Now profiling ' + currentFilePath);
-			runScalene(currentFilePath);	
-		} else {
-			vscode.window.showInformationMessage(`Scalene: ${currentFilePath} is not a Python file.`);
-		}
+      if (currentEditor) {
+        currentFilePath = currentEditor.document.uri.fsPath;
+      } else {
+        return;
+      }
 
-	});
+      // Check whether the file ends in ".py"
+      let isPython = currentFilePath.endsWith(".py");
 
-	context.subscriptions.push(disposable);
+      if (isPython) {
+        runScalene(currentFilePath, context);
+      } else {
+        vscode.window.showInformationMessage(
+          `Scalene: ${currentFilePath} is not a Python file.`,
+        );
+      }
+    },
+  );
+
+  context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
 function deactivate() {}
 
 module.exports = {
-	activate,
-	deactivate
-}
+  activate,
+  deactivate,
+};
